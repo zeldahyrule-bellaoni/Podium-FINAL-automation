110a053eaffb4f897eb9930e5024bcd2f792890c
zeldahyrule-bellaoni <hyrulezelda504@gmail.com>
2026-02-14 06:29:13 +0530

    Update rate-and-message-multiple-ladies.js

--- rate-and-message-multiple-ladies.js
+++ rate-and-message-multiple-ladies.js
@@ -7,9 +7,9 @@ module.exports = async function runRateAndMessageMultipleLadies(page, tierConfig
     // add all names you want to exclude
   ].map(n => n.toLowerCase())); // convert to lowercase for case-insensitive matching
 
-  const m1 = 'A smile is a facelift that’s in everyone’s price range!	Big hugs and max stars 4U	(づ ◕‿◕ )づ';
-  const m2 = 'A smile is a facelift that’s in everyone’s price range!	Big hugs 4U	(づ ◕‿◕ )づ'; //already won
-  const m3 = 'A smile is a facelift that’s in everyone’s price range!	Big hugs 4U	(づ ◕‿◕ )づ'; //168
+  const m1 = 'Love is friendship set on fire - Jeremy Taylor. Happy Valentines day my frnd. xoxo (｡♥‿♥｡)';
+  const m2 = 'Love is friendship set on fire - Jeremy Taylor. Happy Valentines day my frnd. xoxo (｡♥‿♥｡)'; //already won
+  const m3 = 'Love is friendship set on fire - Jeremy Taylor. Happy Valentines day my frnd. xoxo (｡♥‿♥｡)'; //168
 
   const tabLabel = page._guid || 'T?'; //internal tab label in playwright
 
