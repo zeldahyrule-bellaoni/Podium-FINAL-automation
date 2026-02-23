a10ab74e3f2653788b9383eb04a59d38b6d6bbd9
zeldahyrule-bellaoni <hyrulezelda504@gmail.com>
2026-02-24 02:26:01 +0530

    Update rate-and-message-multiple-ladies.js

--- rate-and-message-multiple-ladies.js
+++ rate-and-message-multiple-ladies.js
@@ -7,9 +7,9 @@ module.exports = async function runRateAndMessageMultipleLadies(page, tierConfig
     // add all names you want to exclude
   ].map(n => n.toLowerCase())); // convert to lowercase for case-insensitive matching
 
-  const m1 = 'We are made of star-stuff - Carl Sagan.	Keep shining my frnd! xoxo	₍ᐢ. .ᐢ₎ ₊˚⊹♡';
-  const m2 = 'We are made of star-stuff - Carl Sagan.	Keep shining my frnd! xoxo	₍ᐢ. .ᐢ₎ ₊˚⊹♡'; //already won
-  const m3 = 'We are made of star-stuff - Carl Sagan.	Keep shining my frnd! xoxo	₍ᐢ. .ᐢ₎ ₊˚⊹♡'; //168
+  const m1 = 'We are made of star-stuff - Carl Sagan.	Shine so hard they need sunglasses! xoxo	₍ᐢ. .ᐢ₎ ₊˚⊹♡';
+  const m2 = 'We are made of star-stuff - Carl Sagan.	Shine so hard they need sunglasses! xoxo	₍ᐢ. .ᐢ₎ ₊˚⊹♡'; //already won
+  const m3 = 'We are made of star-stuff - Carl Sagan.	Shine so hard they need sunglasses! xoxo	₍ᐢ. .ᐢ₎ ₊˚⊹♡'; //168
 
   const tabLabel = page._guid || 'T?'; //internal tab label in playwright
 
